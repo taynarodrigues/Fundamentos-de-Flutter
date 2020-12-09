39c34d33e679ef7f6c86a822b16d855ef55c2137
Tayna Rodrigues <taynajesus@gmail.com>
2020-12-09 09:55:17 -0400

    primeiros passos com Flutter

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Column(
+      children: <Widget>[
+        Text(
+          'Deliver features faster',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Column(
+          children: <Widget>[
+            Text(
+              'Craft beatiful UIs',
+              textDirection: TextDirection.ltr,
+            ),
+          ],
+        )
+      ],
+    ));
